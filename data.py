4b1c8ae825eba67e9bfa644111177515cb5cd897
ChenFangM <ChenFang.NY@gmail.com>
2023-03-14 00:45:11 -0400

    Pyandas is so cool

--- data.py
+++ data.py
@@ -59,23 +59,117 @@ df2 = pd.DataFrame(
 print(df2)
 print("\n")
 
-# Data Types
-print(df2.dtypes)
-print("\n")
+# # Data Types
+# print(df.dtypes)
+# print("\n")
 
-# Heads and Tails
-print(df2.head(1))
-print(df2.tail(1))
-print("\n")
+# # Heads and Tails
+# print(df.head(1))
+# print(df.tail(1))
+# print("\n")
+
+# # Index and Columns
+# print(df2.index)
+# print(df2.columns)
+# print("\n")
 
 """
-Operations on DataFrames or Series
+OPERATIONS on DataFrames or Series
+Exclude missing data
+
+To copy, df.copy()
+
+Stats
+df.mean() or df.mean(1) on respective axis
+df.sum
+s.shift(<num>)
+df.apply(lambda x: <val>) appies a user defined function to the data
+s.value_counts() counts values in histogram style
+
+String methods
+s.str.lower() maks all strings lowercase
+
+pd.concat(<array of pieces>) to concatenate pandas objects
+pd.merge(<obj1>, <obj2>, on=<key>) to SQL style join types along specific columns
+
+Grouping methods
+df.groupby(<label>) or df.groupby(<ordered array of labels>)
+df.stack() and df.unstack()
+
+category (a dtype)
+df[<label>].astype("category")
 
 """
 
-# max()
-print(s.max())
-print(df["Age"].max())
+# # max()
+# print(s.max())
+# print(df["Age"].max())
+# print("\n")
 
-# describe()
-print(df.describe())
+# # describe()
+# print(df.describe())
+# print("\n")
+
+# # to_numpy() --- index and column labels trashed
+# """ Note that this can be an expensive operation when your DataFrame has columns with 
+# different data types, which comes down to a fundamental difference between pandas 
+# and NumPy: NumPy arrays have one dtype for the entire array, while pandas DataFrames 
+# have one dtype per column. Function may end up having to cast every value to a Python 
+# object. """
+# array = df.to_numpy()
+# print(array)
+
+# # T() -- transpose
+# print(df.T)
+# print("\n")
+
+# # sort_index(axis=<axis>) 
+# print(df.sort_index(axis=0, ascending=False))
+# print("\n")
+
+# # sort_values(by=<label>)
+# print(df.sort_values(by="Name"))
+# print("\n")
+
+"""
+SELECTION
+
+df["Name"] is the same as df.Name, yielding a Series
+df[index1:index2] slices the rows (inclusive index1, exclusive index2)
+df.loc[<index>] or df.at[<index>, <label>] gets a cross section using a label
+df.iloc[<index1>, <index2>] or df.iloc[<indexRange1>, <indexRange2>] selects by position
+"""
+
+# Boolean Indexing
+print(df[df["Age"] < 18])
+print("\n")
+
+# Note: All values must be be able to pass through the boolean
+# NaN where values do not satisfy the boolean condition
+print(df2[df2 > 0])
+print("\n")
+
+# isin() - Value is in the row
+print(df[df["Age"].isin([16, 18])])
+
+"""
+SETTING VALUES
+
+By label, df2.at[<index>, <label2>] = <value>
+By position, df2.iat[<index1>, <index2>] = <value>
+To a numPy array, df.loc[<label> :, <label>] = <np.array>
+
+"""
+
+# at()
+# df.at[1, "Age"] = 20
+# print(df)
+
+"""
+MISSING DATA
+
+df.dropna() drops rows with missing data
+df.fillna(value=<value>) fills missing data
+isna() gets the boolean mask where values are nan
+
+"""
